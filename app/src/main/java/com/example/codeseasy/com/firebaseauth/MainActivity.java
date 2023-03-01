95e1a97a002213a78266911ae625dfc3c162ab92
abdulthegr8 <95657140+abdulthegr8@users.noreply.github.com>
2023-03-01 17:05:58 -0600

    update search bar refresh

--- app/src/main/java/com/example/codeseasy/com/firebaseauth/MainActivity.java
+++ app/src/main/java/com/example/codeseasy/com/firebaseauth/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ProgressBar;
 
@@ -54,9 +55,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
+                EditText searchBar = findViewById(R.id.search_bar);
+                searchBar.setText("");
                 recreate();
             }
         });
+
     }
 
     @Override
